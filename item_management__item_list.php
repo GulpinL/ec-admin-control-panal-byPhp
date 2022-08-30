5b807f1dc8ca9efdc4465ce9c1b26e68571e0e3e
unknown <halong9c1@gmail.com>
2022-08-30 15:49:10 +0700

    make it better2

--- item_management__item_list.php
+++ item_management__item_list.php
@@ -4,6 +4,7 @@
                 $ch = curl_init();
                 curl_setopt($ch, CURLOPT_URL, $url);
                 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+                curl_setopt( $ch, CURLOPT_USERPWD, ":yesthisismyrealapikeyanditworks");
                 $response = curl_exec($ch);
                 $data = json_decode($response, true);
    
