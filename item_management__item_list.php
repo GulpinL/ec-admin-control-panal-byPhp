4da7bd66b7828acc81b436de360dd4f832ac1b40
unknown <halong9c1@gmail.com>
2022-08-30 15:31:25 +0700

    make it better

--- item_management__item_list.php
+++ item_management__item_list.php
@@ -1,27 +1,15 @@
-<?php
+            <?php  
                 // $url = "http://localhost:9000/api/v1/product/getAllProduct";
                 $url = "https://ezcamp-api-list.herokuapp.com/api/v1/product/getAllProduct";
-                // $data_array = array(
-                //     'token' =>     $_SESSION["key"] 
-                // );
-                // $data=http_build_query($data_array);
-                
                 $ch = curl_init();
-                
                 curl_setopt($ch, CURLOPT_URL, $url);
-                // curl_setopt($ch, CURLOPT_POST, true);
-                // curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
                 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-                // curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);
-                // curl_setopt($ch, CURLOPT_TIMEOUT , 10);
-                
                 $response = curl_exec($ch);
-                
                 $data = json_decode($response, true);
    
                 curl_close($ch);
                 
-?>
+            ?>
                 <p class="text-center">WFH  RECORD</p>
                 <div class="table-responsive text-nowrap">
                   <table class="table">
